8a3b1e6b32076fa5336845abc84ac0f961b2695d
Chapman Siu <chapm0n.siu@gmail.com>
2014-01-28 22:29:01 +0000

    refactored data_parse.py

--- stan/data/data_parse.py
+++ stan/data/data_parse.py
@@ -1,82 +1,6 @@
 # sas parse.py
 
 from data_lex import dataStepStmt
-import pandas as pd
-from numpy import random
-
-info = dataStepStmt.parseString("""data test;set df; run;""")
-
-from pandas import DataFrame
-
-df = DataFrame({
-         'x': random.uniform(1., 168., 120),
-         'y': random.uniform(7., 334., 120),
-         'z': random.uniform(1.7, 20.7, 120),
-         'month': [5,6,7,8]*30,
-         'week': random.randint(1,4, 120)
-     })
-
-# figure out the parsing rules
-"""data test;
-set df; 
-run;"""
-
-#pandas solution
-"""
-test = df
-"""
-
-# simplest test case
-print ''.join([info.data[0], "=", info.set[0]])
-
-# figure out the parsing rules
-
-#pandas solution
-"""
-test = df.rename(columns={"z": "b"})
-"""
-
-# you probably have to figure the keys first and then act accordingly, unfortunately in this format
-# its difficult to know whether options refer to `data` or `set`
-
-#info1.keys() <- using the keys determine how it should be parsed..
-
-# also need to determine precedence, the easiest way might be to set up a class?
-# to do precedence, one method might be to sort the the keys by precedence and used that as the 
-# basis for ordering
-
-# since you have a dict, you could just use a dict of functions....
-
-# you would probably want to use decorators, since you will have the base thing and continually wrap things around it?
-# ....or maybe not, since you just want to call functions down a stack
-
-# or you could just have a list of try except, or get functions to determine it. 
-
-# --------------------------------------------------------------------
-# loose order
-
-# 1. set statement options (inline)
-# 2. Non-inline options
-# 3. the statments in the order that is parsed
-# 4. data statement options (inline)
-
-cstr = """data test(rename=(b = z));
-set df (rename=(z = b) drop= x y);
-t = "chapman";
-g = -1;
-c = 1+2;
-d = b+c;
-na = substr(t,1,1) + "a";
-halfyr = if month < (6*180) then 1 else if month > (6*180) then 2 else 0;
-run;"""
-
-
-
-print "Loading..."
-
-
-
-print "Computing String...\n"
 
 def id_convert(v_ls, data):
     # this statement needs to have testing
@@ -102,10 +26,6 @@ def logic_convert(v_ls, data):
     else:
         lmd = id_convert(v_ls, 'x')
     return lmd
-
-def substr(ss, start, length):
-    return ss[start-1:start+length-1]
-
 
 def stan(cstr):
     inf = dataStepStmt.parseString(cstr)
@@ -169,5 +89,3 @@ def stan(cstr):
 
     return ss
 
-
-print "Soln is : \n\n", stan(cstr)
