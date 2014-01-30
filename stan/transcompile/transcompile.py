6eeca00995bdc281a63ac585b9061973daebd89e
Chapman Siu <chapm0n.siu@gmail.com>
2014-01-30 23:13:01 +0000

    Added support for comments, (multiline and single + python code)

--- stan/transcompile/transcompile.py
+++ stan/transcompile/transcompile.py
@@ -9,6 +9,31 @@ from stan.proc.proc_parse import proc_parse
 
 
 def transcompile(cstr):
+    """transcompile converts a stan script to python equivalent
+    
+    Parameters
+    ----------
+    
+    cstr : the stan script in string format
+    
+    Notes 
+    -----
+    
+    Comments are stripped out first, and then all python code is ignored.
+    
+    Multi-line comments start with ``/*`` and end with ``*/``
+    Single line comments start with ``*`` and end with ``;`` or start with ``//``
+    Python code begin with ``#``
+    
+    """
+    multi_comments = re.compile(r'/\*.*?\*/', re.DOTALL)
+    single_c_comments = re.compile(r'//.*?')
+    single_sas_comments = re.compile(r'\*.*?;')
+    
+    cstr = multi_comments.sub(r' ', cstr)
+    cstr = single_c_comments.sub(r' ', cstr)
+    cstr = single_sas_comments.sub(r' ', cstr)    
+    
     find_rule = re.compile(r'(((?:data)|(?:proc)).*?(run\s*;))', re.I | re.DOTALL)
     f_all = find_rule.findall(cstr)
     
@@ -22,5 +47,3 @@ def transcompile(cstr):
             raise "invalid code found"
     
     return re.sub(r'\n+', '\n', ss)
-
-
