9be8bd91e5308b0840192712a76652788d333f35
chapman siu <chapm0n.siu@gmail.com>
2014-02-01 13:37:09 +1100

    added documentation
    github.io/chappers/Stan

--- stan/proc_functions/merge.py
+++
@@ -1,6 +0,0 @@
-"""
-The :mod:`stan.proc_functions.merge` module is the proc merge function
-"""
-
-def merge(dt_left, dt_right, how='inner', on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True):
-    return dt_left.merge(dt_right, how='inner', on=None, left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True)
